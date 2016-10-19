4a1ab171e1ba5ebcba8a3db38fc1f1e60ceb1320
Shashank Khandelwal <shashank.khandelwal@gsa.gov>
2016-10-19 18:14:08 +0000

    Add www to URLs if the first attempt 404s

--- scraper/fedtext/spiders/tutorial_spider.py
+++ scraper/fedtext/spiders/tutorial_spider.py
@@ -1,9 +1,13 @@
-import scrapy
+from urllib.parse import urlparse
 import csv
+import sys
+
+import scrapy
+from scrapy import Request
+
 from bs4 import BeautifulSoup
 from bs4.element import Comment, Doctype
 
-from scrapy.http.request import Request
 
 from fedtext.items import FedtextItem
 
@@ -20,11 +24,17 @@ def visible(element):
     else:
         return element.strip()
 
+def add_www(original_url):
+    """ Take a URL such as http://18f.gov and return http://www.18f.gov """
+
+    o = urlparse(original_url)
+    return "http://www.{}".format(o.netloc)
+
 
 class TutorialSpider(scrapy.Spider):
     name = "tutorialspider"
-    allowed_domains = ['*.gov']
     start_urls = []
+    handle_httpstatus_list = [404]
 
     def __init__(self, *args, **kwargs):
         super(TutorialSpider, self).__init__(*args, **kwargs)
@@ -47,17 +57,26 @@ class TutorialSpider(scrapy.Spider):
             Callback method for parsing the response. Yields a FedtextItem.
         """
 
-        soup = BeautifulSoup(response.body_as_unicode(), 'lxml')
-        texts = soup.findAll(text=True)
+        if response.status == 404:
+            original_url = response.url
+            url = add_www(original_url)
+            yield Request(url)
+        else:
+            soup = BeautifulSoup(response.body_as_unicode(), 'lxml')
+            texts = soup.findAll(text=True)
 
-        # Remove duplicate statements, only grab what's visible
-        visible_texts = list(set([t.strip() for t in texts if visible(t)]))
-        
-        item = FedtextItem()
-        item['text_list'] = visible_texts
-        item['word_list'] = []
-        item['word_frequency'] = []
-        item['link'] = response.url
-        item['title'] = soup.title.text
+            if soup.title:
+                item = FedtextItem()
+                # Some pages, like safetyact.gov don't have well-formed markup without 
+                # JavaScript. 
+                item['title'] = soup.title.text
+                item['text_list'] = [t.strip() for t in texts if visible(t)]
+                item['word_list'] = []
+                item['word_frequency'] = []
+                item['response_url'] = response.url
 
-        yield item
+                if 'request_url' in response.request.meta:
+                    item['request_url'] = response.request.meta['redirect_urls'][0]
+                else:
+                    item['request_url'] = response.url
+                yield item
